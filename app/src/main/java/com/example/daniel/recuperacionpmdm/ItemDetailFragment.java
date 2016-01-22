78673b167b06beec67063bcb5ec56b06ee65cf3e
dpereiracarrillo <dpereiracarrillo@danielcastelao.org>
2016-01-22 12:00:56 +0100

    hago el método del botón en el onCreateView del ItemDetailFragment y borro los comentarios del mismo

--- app/src/main/java/com/example/daniel/recuperacionpmdm/ItemDetailFragment.java
+++ app/src/main/java/com/example/daniel/recuperacionpmdm/ItemDetailFragment.java
@@ -1,38 +1,27 @@
 package com.example.daniel.recuperacionpmdm;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.daniel.recuperacionpmdm.dummy.DummyContent;
 
-/**
- * A fragment representing a single Item detail screen.
- * This fragment is either contained in a {@link ItemListActivity}
- * in two-pane mode (on tablets) or a {@link ItemDetailActivity}
- * on handsets.
- */
+
 public class ItemDetailFragment extends Fragment {
-    /**
-     * The fragment argument representing the item ID that this fragment
-     * represents.
-     */
+
     public static final String ARG_ITEM_ID = "item_id";
 
-    /**
-     * The dummy content this fragment is presenting.
-     */
+
     private DummyContent.DummyItem mItem;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
+
     public ItemDetailFragment() {
     }
 
@@ -41,9 +30,7 @@ public class ItemDetailFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         if (getArguments().containsKey(ARG_ITEM_ID)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
+
             mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
 
             Activity activity = this.getActivity();
@@ -57,12 +44,30 @@ public class ItemDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
 
-        // Show the dummy content as text in a TextView.
+        
         if (mItem != null) {
             ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.details);
+            Button boton = (Button) rootView.findViewById(R.id.botonBorrar);
+            boton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ItemListFragment fragment = (ItemListFragment) getFragmentManager().findFragmentById(R.id.item_list);
+                    if (fragment == null || !fragment.isInLayout()) {
+                        Intent intent = new Intent();
+                        getActivity().setResult(Activity.RESULT_OK, intent);
+                        getActivity().finish();
+                    }else {
+                        ((TextView) rootView.findViewById(R.id.item_detail)).setText("");
+
+                    }
+
+                }
+            });
         }
+
+
 
         return rootView;
     }
